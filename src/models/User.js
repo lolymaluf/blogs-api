fce047ce85bd55161f4df9264b50700163a41057
Loly Maluf <lolymaluf@gmail.com>
2022-09-30 00:06:58 -0300

    Adiciona Requisito 02

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    displayName: {
+      field: 'display_name',
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  }, 
+  {
+    timestamps: false,
+    tablename: 'users',
+    underscored: true,
+  });
+
+  return User;
+};
+
+module.exports = User;
